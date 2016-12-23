3a612a67849a51268c7a16ddf5b379c676431790
unknown <leifengyao@vip.qq.com>
2016-12-23 15:48:25 +0800

    some modify

--- php.go
+++ php.go
@@ -1,1 +1,14 @@
 package php
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+func Md5(s string) string {
+	md5Ctx := md5.New()
+	md5Ctx.Write([]byte(s))
+	md5Hash := md5Ctx.Sum(nil)
+	md5Str := string(hex.EncodeToString(md5Hash))
+	return md5Str
+}
